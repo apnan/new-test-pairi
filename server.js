89cbc31ebf2750497156d7cb1469866243bb41f7
apnan <aishajahan2015@gmail.com>
2023-01-16 15:42:42 +0200

    added code to service and server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-var favicon = require('serve-favicon')
-var path = require('path')
+const express = require('express');
+const app = express();
+app.use(express.json());
 
+module.exports = app;
 const PORT = process.env.PORT || 3000
-
-const app = express()
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(express.static('public'))
 
-app.listen(PORT, console.log(`http server listening on port ${PORT}`))
+app.listen(PORT, () => {
+    console.log(`http server listening on port ${PORT}`)
+});
