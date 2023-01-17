dc42861f1aef0df6c47ee5d1bc2ffa6857b75520
apnan <aishajahan2015@gmail.com>
2023-01-17 09:32:11 +0200

    Added test frameworks

--- server.js
+++ server.js
@@ -3,9 +3,3 @@ const app = express();
 app.use(express.json());
 
 module.exports = app;
-const PORT = process.env.PORT || 3000
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-
-app.listen(PORT, () => {
-    console.log(`http server listening on port ${PORT}`)
-});
